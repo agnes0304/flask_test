a6b805685bbff3c75ea1de208b360bc4222329f5
agnes0304 <jiwoochoi0304@gmail.com>
2022-02-11 17:44:40 +0900

    ongoing

--- app.py
+++ app.py
@@ -13,6 +13,23 @@ with open('student_sample.json') as json_file:
     json_data = json.load(json_file)
 
 
+def islist(name):
+    name_list= list()
+    for i in range(len(json_data)):
+        name_list.append(json_data[i]["name"])
+
+    if name in name_list:    
+        return True
+    else:
+        return False
+
+def find_idx(name):
+    name_list= list()
+    for i in range(len(json_data)):
+        name_list.append(json_data[i]["name"])
+    idx = int(name_list.index(name))
+    return idx
+
 @app.route("/")
 def Welcome():
     return "<p>Welcome!</p>"
@@ -25,21 +42,23 @@ def randomStudent():
 
 @app.route("/student/<name>", methods=["GET"])
 def get_student(name):
-    name_list= list()
-    for i in range(len(json_data)):
-        name_list.append(json_data[i]["name"])
-
-    if name in name_list:
-        idx = int(name_list.index(name))
-        return f'{name} is in the list! ID is {idx}'
+    if islist(name):
+        idx = find_idx(name)
+        return f'{name} is in the list! ID is {idx+1}'
+    
     else:
         return f'{name} is not in the list.'
 
 
 # ongoing
-@app.route("/student/<id>", methods=["PUT"])
-def update_student(id):
-    return "PUT"
+@app.route("/student/<name>/<id>", methods=["PUT"])
+def update_student(name, id):
+    if islist(name):
+        idx = find_idx(name)
+        json_data[idx]["id"] = id
+        return f'ID of {name} is updated to {id}'
+    else:
+        return "enter another right name and id."
 
 
 @app.route("/student/<name>", methods=["POST"])
@@ -51,21 +70,16 @@ def add_student(name):
 
 @app.route("/student/<name>/<id>", methods=["DELETE"])
 def delete_student(name, id):
-    name_list= list()
-    for i in range(len(json_data)):
-        name_list.append(json_data[i]["name"])
-    
-    idx = int()
-    if name in name_list:
-        idx = name_list.index(name)
-
+    if islist(name):
+        idx = find_idx(name)
+        # print(idx)
+        # print(json_data[idx])
+        # print(json_data[idx]["id"])
         if json_data[idx]["id"] == id :
             del json_data[idx]
-            return "Delete"
-        else : 
-            return "Enter another name or id."
-# else의 경우 제대로 출력됨.
-# 학생 리스트에 있는 name과 id 입력 시, TypeError
+    else : 
+        return "Enter another name or id."
+# "/student/h/7"의 idx, json_data[idx] 출력 결과 : 7, {'name':'h', 'id':8} 제대로 나옴
 
 
 # 현재 시간을 반환
