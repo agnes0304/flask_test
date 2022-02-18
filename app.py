deef1a478e2915cf73ef42649a0ea5b80faf5e16
agnes0304 <jiwoochoi0304@gmail.com>
2022-02-19 01:22:50 +0900

    ongoing

--- app.py
+++ app.py
@@ -1,28 +1,40 @@
-from crypt import methods
+# test_flask
+# pw에 hash 적용하기
+# subject 리스트에 과목 추가하기
+# id 랜덤하게 생성하기
+# nickname 추가하기
+
+
 import datetime
-from fcntl import lockf
 import json
-from os import name
 import random
-from unittest import result
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-with open('student_sample.json', 'a') as json_file:
+
+with open('student_sample.json', 'r+') as json_file:
     json_data = json.load(json_file)
 
+# 바로 close 하는지 확인
 
 def islist(name):
     name_list = list()
     for i in range(len(json_data)):
         name_list.append(json_data[i]["name"])
-
     if name in name_list:
         return True
     else:
         return False
 
+def isID(id):
+    id_list = list()
+    for i in range(len(json_data)):
+        id_list.append(json_data[i]["id"])
+        if id in id_list:
+            return True
+        else:
+            return False
 
 def find_idx(name):
     name_list = list()
@@ -31,61 +43,86 @@ def find_idx(name):
     idx = int(name_list.index(name))
     return idx
 
+def find_id(name):
+    for i in range(len(json_data)):
+        if name == json_data[i]["name"]:
+            return json_data[i]["id"]
+
 
 @app.route("/")
 def Welcome():
     return "<p>Welcome!</p>"
 
-
+# whole data
 @app.route("/student")
-def randomStudent():
-    return json_data[random.randrange(0, len(json_data))]
+def student():
+    return json_data
 
 
 @app.route("/student/<name>", methods=["GET"])
 def get_student(name):
     if islist(name):
         idx = find_idx(name)
-        return f'{name} is in the list! ID is {idx+1}'
+        return json_data[idx]
 
     else:
         return f'{name} is not in the list.'
 
 
-# ongoing
+# update; name and id check
 @app.route("/student/<name>/<id>", methods=["PUT"])
 def update_student(name, id):
     if islist(name):
         idx = find_idx(name)
-        json_data[idx]["id"] = id
-        json_data.dump(json_data, json_file)
-        return f'ID of {name} is updated to {id}'
+        if json_data[idx]["id"] == id:
+            body = request.get_json()
+            # nickname&bio, only nickname, only bio 구분?
+            nickname = body["nickname"]
+            bio = body["bio"]
+
+            json_data[idx]["nickname"] = nickname
+            json_data[idx]["bio"] = bio
     else:
         return "enter another right name and id."
+    # file write 해줘야 함.  
 
 
+# name, nickname, bio만 request body에 json으로
+# id = random
+# subject = empty list
 @app.route("/student/<name>", methods=["POST"])
 def add_student(name):
-    json_data[len(json_data)-1]["name"] = name
-    json_data[len(json_data)-1]["id"] = random.randrange(0, 100)
-    return f'{name} is added in the list'
+    body = request.get_json()
 
+    nickname = body["nickname"]
+    bio = body["bio"]
+
+    json_data[len(json_data)]["name"] = name
+    json_data[len(json_data)]["nickname"] = nickname
+    json_data[len(json_data)]["bio"] = bio
+    json_data[len(json_data)]["subject"] = []
+
+    new_id = random.randint(1,1000)       
+    while isID(new_id):              # a가 이미 뽑은 리스트에 있을 때까지 다시 뽑자
+        new_id = random.randint(1,10000)
+        return new_id    
+    json_data[len(json_data)]["id"] = new_id
+
+    # json file 다시 써줘야 함. 
+    
 
 @app.route("/student/<name>/<id>", methods=["DELETE"])
 def delete_student(name, id):
     if islist(name):
         idx = find_idx(name)
-        # print(idx)
-        # print(json_data[idx])
-        # print(json_data[idx]["id"])
         if json_data[idx]["id"] == id:
             del json_data[idx]
     else:
         return "Enter another name or id."
-# "/student/h/7"의 idx, json_data[idx] 출력 결과 : 7, {'name':'h', 'id':8} 제대로 나옴
+
+    # file write 다시
 
 
-# 현재 시간을 반환
 @app.route("/time")
 def now():
     now = datetime.datetime.now()
