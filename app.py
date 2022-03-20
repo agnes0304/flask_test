1a4c536d399c514280fc097b302a56e7ccb3305f
agnes0304 <jiwoochoi0304@gmail.com>
2022-03-20 15:01:57 +0900

    ongoing

--- app.py
+++ app.py
@@ -2,13 +2,11 @@
 #           Create, Read, Update, Delete
 # Method :: POST, GET, PUT, DELETE
 
-# 유저 정보로 hash된 id 생성
 
+from crypt import methods
 import datetime
-import email
 import hashlib
 import json
-from xmlrpc.client import Boolean
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -32,11 +30,12 @@ def student():
 
 
 # 입력한 name이 student data에 있는지 확인
-def islist(name):
+def isName(name):
     name_list = list()
     json_data = openjson()
+    values = json_data.values()
     for i in range(len(json_data)):
-        name_list.append(json_data[i]["name"])
+        name_list.append(values[i]["name"])
     if name in name_list:
         return True
     else:
@@ -49,64 +48,68 @@ def makeID(email):
     print(id)
 
 
-# name으로 index와 id 찾는 함수 -> id를 key로 설정하면 수정 필요
-def find_idx(name: str):
-    json_data = openjson()
+# name으로 id(key)찾는 함수 필요
+def idByName(name):
     name_list = list()
+    json_data = openjson()
+    values = list(json_data.values())
+    keys = list(json_data.keys())
     for i in range(len(json_data)):
-        name_list.append(json_data[i]["name"])
-    idx = int(name_list.index(name))
-    return idx
+        name_list.append(values[i]["name"])
+    id = keys[name_list.index(name)]
+    return id
 
 
-def find_id(name):
-    for i in range(len(json_data)):
-        if name == json_data[i]["name"]:
-            return json_data[i]["id"]
+# def find_id(name):
+#     for i in range(len(json_data)):
+#         if name == json_data[i]["name"]:
+#             return json_data[i]["id"]
 
 
 @app.route("/")
 def Welcome():
     return "<p>Welcome!</p>"
 
-@app.route("/studentByName/<name>", methods=["GET"])
+@app.route("/student/<name>", methods=["GET"])
 def get_student_by_name(name):
     json_data = openjson()
-    if islist(name):
-        idx = find_idx(name)
-        return json_data[idx]
+    id = idByName(name)
+    return json_data[id]
 
-    else:
-        return f'{name} is not in the list.'
 
-<<<<<<< HEAD
+@app.route("/studentID/<name>", methods=["GET"])
+def idByName(name):
+    name_list = list()
+    json_data = openjson()
+    values = list(json_data.values())
+    keys = list(json_data.keys())
+    for i in range(len(json_data)):
+        name_list.append(values[i]["name"])
+    id = keys[name_list.index(name)]
+    return id
+
 
 # get student data by ID
 # 근데 id는 랜덤 생성 되고 있음 -> makeID()
-# user : email 입력 -> hash -> get data(?)
-# email로만 hashed id를 생성하는 경우, 아래 id로 data가져오기가 의미가 없지 않나?
-@app.route("/student/<email>", methods=["GET"])
-def get_student_by_id(email):
-    # json_data = openjson()
-    # id = hashlib.md5(bytes(email, 'utf-8')).hexdigest()
-    # print(json_data[id])
-    # return "done"
-    pass
+@app.route("/studentById/<id>", methods=["GET"])
+def get_student_by_id(id):
+    json_data = openjson()
+    data = json_data[id]
+    return data
+
 
 # update student info by ID
-# PUT /student/jiwoochoi0304@gmail.com
-@app.route("/student/<email>", methods=["PUT"])
-def update_student(email):
+# PUT /student/id알고 있다고 가정
+@app.route("/student/<id>", methods=["PUT"])
+def update_student(id):
     body = request.get_json()
     
     key_list = list(body.keys())
     
-    json_data=openjson()
-    
-    id = hashlib.md5(bytes(email, 'utf-8')).hexdigest()
+    json_data = openjson()
     
 
-    # 키 리스트를 돌면서 json_data[id]의 [key]를 body[key]로 업데이트
+    # key_list에 있는 key에 해 json_data[id]의 [key]의 [value]를 body[key]의 [value]로 업데이트
     for i in range(len(key_list)):
         json_data[id][key_list[i]] = body[key_list[i]]
 
@@ -115,24 +118,6 @@ def update_student(email):
 
     return json.dumps(json_data)
 
-=======
-# update; name and id check
-# subject append 하는 거 추가해야 함
-@app.route("/student/<name>/<id>", methods=["PUT"])
-def update_student(name, id):
-    if islist(name):
-        idx = find_idx(name)
-        if json_data[idx]["id"] == id:
-            body = request.get_json()
-            # nickname&bio, only nickname, only bio 구분?
-            nickname = body["nickname"]
-            bio = body["bio"]
-
-            json_data[idx]["nickname"] = nickname
-            json_data[idx]["bio"] = bio
-    else:
-        return "enter another right name and id."
->>>>>>> 26a596d2fcd57ac77940050803ff37df60ddebbf
 
 # Path param, Query param
 # dict = map = hashmap = hashtable
@@ -143,18 +128,14 @@ def update_student(name, id):
 @app.route("/student", methods=["POST"])
 def add_student():
     body = request.get_json()
-    # { "name": "jiwoo", "age": 25, "email": "jiwoochoi0304@gmail.com"}
+    # { "name": "longzero", "age": 26, "email": "longzero@gmail.com"}
 
     id = hashlib.md5(bytes(body["email"], 'utf-8')).hexdigest()
-
+    
     json_data = None
     with open("student_sample.json", 'r') as json_file:
         json_data = json.load(json_file)
-        json_data.append({
-            "id": id,
-            "name": body["name"],
-            "email": body["email"]
-        })
+        json_data[id] = body
 
     with open("student_sample.json", 'w') as json_file:
         json.dump(json_data, json_file)
@@ -163,13 +144,10 @@ def add_student():
 
 
 # DELETE
-@app.route("/student/<email>", methods=["DELETE"])
-def delete_student(email): 
+@app.route("/student/<id>", methods=["DELETE"])
+def delete_student(id): 
     json_data = openjson()
-
-    for i in range(len(json_data)):
-        if json_data[i]['email'] == email:
-            del json_data[i]
+    del json_data[id]
     
     with open("student_sample.json", 'w') as json_file:
         json.dump(json_data, json_file)
@@ -181,3 +159,9 @@ def delete_student(email):
 def now():
     now = datetime.datetime.now()
     return now.strftime("%Y년 %m월 %d일 %H시 %M분")
+
+
+# json_data test
+# json_data = openjson()
+# print(json_data)
+# print(json_data["ffa3e85d4b7673a203ced1e1cc709efb"])
